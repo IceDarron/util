e5f3e79ea844e1e3bc9f9c3ac36be5ccf4ca014f
IceDarron <darron_r@163.com>
2018-07-29 23:41:08 +0800

    修改二维码工具包

--- src/main/java/qr/App.java
+++ src/main/java/qr/App.java
@@ -5,15 +5,15 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         //生成带logo 的二维码
-        String text = "https://www.oschina.net/p/springboot-vue-";
-        QRCodeUtil.encode(text, "./src/main/java/QR/捕11获.PNG", "./src/main/java/QR/", true);
+        String text = "可以了解下，微博链接下评论内容，关注sd1206，长春长生";
+        QRCodeUtil.encode(text, "./src/main/java/qr/V.jpg", "./src/main/java/QR/", true);
 
-        //生成不带logo 的二维码
-        String textt = "http://www.baidu.com";
-        QRCodeUtil.encode(textt, "", "./src/main/java/QR/", true);
-
-        //指定二维码图片，解析返回数据
-        System.out.println(QRCodeUtil.decode("./src/main/java/QR/QR-20180717112532.jpg"));
+//        //生成不带logo 的二维码
+//        String textt = "http://www.baidu.com";
+//        QRCodeUtil.encode(textt, "", "./src/main/java/QR/", true);
+//
+//        //指定二维码图片，解析返回数据
+//        System.out.println(QRCodeUtil.decode("./src/main/java/QR/QR-20180717112532.jpg"));
 
     }
 }
