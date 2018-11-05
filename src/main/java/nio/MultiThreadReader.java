72e12a54508ccfaff810f2eb93ebcb1a110f356a
IceDarron <darron_r@163.com>
2018-11-06 00:26:04 +0800

    增加工具类

--- src/main/java/nio/MultiThreadReader.java
+++ src/main/java/nio/MultiThreadReader.java
@@ -3,12 +3,10 @@ package nio;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class MultiThreadReader implements Runnable {
+
     private FileChannel channel;
     private long startIndex;
     private long endIndex;
@@ -61,17 +59,9 @@ public class MultiThreadReader implements Runnable {
         }
     }
 
-    /**
-     * 功能实现：核心类java.util.concurrent.CountDownLatch
-     * CountDownLatch : 一个线程(或者多个)， 等待另外N个线程完成某个事情之后才能执行。本质上来讲就是一个计数器
-     * 其核心还是AQS，在此不多赘述。
-     */
+
     public static void main(String[] args) {
-
-        CountDownLatch countDownLatch = new CountDownLatch(4); // 设置线程数
-        ExecutorService executorService = Executors.newCachedThreadPool();//线程池
-
-
+        ExecutorService executorService = new ThreadPoolExecutor(5, 10, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue());//线程池
 
     }
 }
