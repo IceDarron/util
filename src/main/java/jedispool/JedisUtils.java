9fe879d7200934d7f8b5bbbb4e20e041da93e06a
rxn <darron_r@163.com>
2019-01-28 10:26:03 +0800

    修改判空

--- src/main/java/jedispool/JedisUtils.java
+++ src/main/java/jedispool/JedisUtils.java
@@ -8,7 +8,7 @@ public class JedisUtils {
 
     public static String getString(Properties properties, String str, String s) {
         boolean isNullOrEmpty = Strings.isNullOrEmpty(properties.getProperty(str));
-        if (isNullOrEmpty) {
+        if (!isNullOrEmpty) {
             s = properties.getProperty(str);
         }
         return s;
@@ -16,7 +16,7 @@ public class JedisUtils {
 
     public static Long getLong(Properties properties, String str, Long l) {
         boolean isNullOrEmpty = Strings.isNullOrEmpty(properties.getProperty(str));
-        if (isNullOrEmpty) {
+        if (!isNullOrEmpty) {
             l = Long.parseLong(properties.getProperty(str).replaceAll("L", "").replaceAll("l", ""));
         }
         return l;
@@ -24,7 +24,7 @@ public class JedisUtils {
 
     public static int getInt(Properties properties, String str, int i) {
         boolean isNullOrEmpty = Strings.isNullOrEmpty(properties.getProperty(str));
-        if (isNullOrEmpty) {
+        if (!isNullOrEmpty) {
             i = Integer.parseInt(properties.getProperty(str));
         }
         return i;
@@ -32,7 +32,7 @@ public class JedisUtils {
 
     public static boolean getBoolean(Properties properties, String str, Boolean bool) {
         boolean isNullOrEmpty = Strings.isNullOrEmpty(properties.getProperty(str));
-        if (isNullOrEmpty) {
+        if (!isNullOrEmpty) {
             bool = Boolean.parseBoolean(properties.getProperty(str));
         }
         return bool;
