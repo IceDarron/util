95fdb0ba7c18fb3f2c5d3b095d048f2022069220
rxn <darron_r@163.com>
2019-01-29 17:22:40 +0800

    分布式锁-redis

--- src/main/java/jedispool/Test1.java
+++ src/main/java/jedispool/Test1.java
@@ -2,6 +2,12 @@ package jedispool;
 
 import redis.clients.jedis.Jedis;
 
+/**
+ * redis 主从，哨兵，集群
+ * https://blog.csdn.net/liuchuanhong1/article/details/53206028
+ * https://www.cnblogs.com/lgeng/p/6623336.html
+ * https://www.cnblogs.com/xifenglou/p/8372447.html
+ */
 public class Test1 {
     public static void main(String[] args) {
         Jedis jedis = JedisPoolManager.getJedis();
