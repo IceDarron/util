1260e9a69ff51d8a65ac4fc3efef73d18e5153bc
rxn <darron_r@163.com>
2019-01-30 11:12:20 +0800

    redis的主从，哨兵，集群测试

--- src/main/java/jedispool/JedisPoolManager.java
+++ src/main/java/jedispool/JedisPoolManager.java
@@ -1,10 +1,7 @@
 package jedispool;
 
 import druiddatasource.PropertiesManager;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.JedisSentinelPool;
+import redis.clients.jedis.*;
 import redis.clients.util.Pool;
 
 import java.util.HashSet;
@@ -13,71 +10,69 @@ import java.util.Set;
 
 public class JedisPoolManager {
 
-    private static Pool<Jedis> sentinelPool;
+    private static Pool<Jedis> pool;
 
-    static {
-        poolInit();
-    }
+    private static JedisCluster clusterPool;
 
-    private static void createJedisPool() {
-        //利用双端检测方式来保证只有一个jedispool
-        if (sentinelPool != null)
-            return;
-
+    private static void createPool() {
         // 加载配置文件
         Properties prop = PropertiesManager.LOAD_PROPERTIES("redis.properties");
+
+        // common
+        int maxTotal = JedisUtils.getInt(prop, "maxTotal", 20);
+        int maxIdle = JedisUtils.getInt(prop, "maxIdle", 20);
+        int minIdle = JedisUtils.getInt(prop, "minIdle", 5);
+        boolean testOnBorrow = JedisUtils.getBoolean(prop, "testOnBorrow", false);
+        boolean testOnReturn = JedisUtils.getBoolean(prop, "testOnReturn", false);
+        boolean testWhileIdle = JedisUtils.getBoolean(prop, "testWhileIdle", true);
+        long maxWaitMillis = JedisUtils.getLong(prop, "maxWaitMillis", 5000L);
         int timeout = JedisUtils.getInt(prop, "timeout", 5000);
-        // 是否单节点
-        boolean isSingle = JedisUtils.getBoolean(prop, "isSingle", true);
-        String host = JedisUtils.getString(prop, "host", "127.0.0.1");
-        int port = JedisUtils.getInt(prop, "port", 6379);
-        String masterName = JedisUtils.getString(prop, "masterName", "mymaster");
         String password = JedisUtils.getString(prop, "password", null);
-        Set<String> sentinelSet = new HashSet<String>();
-        if (prop.containsKey("sentinels")) {
-            String sentinels = prop.getProperty("sentinels");
-            String[] hostList = sentinels.split(";");
-            for (String h : hostList) {
-                sentinelSet.add(h);
-            }
+        String model = JedisUtils.getString(prop, "model", null);
+
+        // master_slave_node
+        String master_slave_host = JedisUtils.getString(prop, "master_slave_node", "master_slave_node").split(":")[0];
+        int master_slave_port = Integer.parseInt(JedisUtils.getString(prop, "master_slave_node", "master_slave_node").split(":")[1]);
+
+        // sentinel
+        String masterName = JedisUtils.getString(prop, "masterName", "master");
+        String sentinel = JedisUtils.getString(prop, "sentinel_node", null);
+        Set<String> sentinelNode = new HashSet<String>();
+        String[] sentinelArray = sentinel.split(",");
+        for (String s : sentinelArray) {
+            sentinelNode.add(s);
         }
+
+        // cluster
+        String cluster = JedisUtils.getString(prop, "cluster_node", null);
+        String[] clusterArray = cluster.split(",");
+        Set<HostAndPort> clusterNode = new HashSet<>();
+        for (String s : clusterArray) {
+            String[] arr = s.split(":");
+            clusterNode.add(new HostAndPort(arr[0], Integer.valueOf(arr[1])));
+        }
+        int soTimeout = JedisUtils.getInt(prop, "so_timeout", 2000);
+        int maxRedirections = JedisUtils.getInt(prop, "max_redirections", 10);
 
         // 建立连接池配置参数
         JedisPoolConfig config = new JedisPoolConfig();
-        config.setMaxTotal(JedisUtils.getInt(prop, "maxTotal", 20));
-        config.setMaxIdle(JedisUtils.getInt(prop, "maxIdle", 20));
-        config.setMinIdle(JedisUtils.getInt(prop, "minIdle", 5));
-        config.setTestOnBorrow(JedisUtils.getBoolean(prop, "testOnBorrow", false));
-        config.setTestOnReturn(JedisUtils.getBoolean(prop, "testOnReturn", false));
-        config.setMaxWaitMillis(JedisUtils.getLong(prop, "maxWaitMillis", 5000L));
-        config.setTestOnReturn(JedisUtils.getBoolean(prop, "testWhileIdle", true));
+        config.setMaxTotal(maxTotal);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+        config.setTestOnReturn(testWhileIdle);
+        config.setMaxWaitMillis(maxWaitMillis);
 
-
-        if (isSingle) {
-            if (JedisUtils.isNull(password)) {
-                //单机无密码
-                sentinelPool = new JedisPool(config, host, port, timeout);
-            } else {
-                //单机有密码
-                sentinelPool = new JedisPool(config, host, port, timeout, password);
-            }
+        if ("master_slave".equals(model)) {
+            pool = new JedisPool(config, master_slave_host, master_slave_port, timeout, password);
+        } else if ("sentinel".equals(model)) {
+            pool = new JedisSentinelPool(masterName, sentinelNode, config, timeout, password);
+        } else if ("cluster".equals(model)) {
+            clusterPool = new JedisCluster(clusterNode, timeout, soTimeout, maxRedirections, config);
         } else {
-            if (JedisUtils.isNull(password)) {
-                //集群无密码
-                sentinelPool = new JedisSentinelPool(masterName, sentinelSet, config, timeout);
-            } else {
-                //集群有密码
-                sentinelPool = new JedisSentinelPool(masterName, sentinelSet, config, timeout, password);
-            }
+            System.out.println("请选择创建的redis模式");
         }
-    }
-
-    /**
-     * 在多线程环境同步初始化
-     */
-    private static synchronized void poolInit() {
-        if (sentinelPool == null)
-            createJedisPool();
     }
 
     /**
@@ -86,7 +81,32 @@ public class JedisPoolManager {
      * @return
      */
     public static Jedis getJedis() {
-        return sentinelPool.getResource();
+        if (pool == null) {
+            synchronized (JedisPoolManager.class) {
+                if (pool == null) {
+                    createPool();
+                    System.out.println("创建jedis连接池完毕");
+                }
+            }
+        }
+        return pool.getResource();
+    }
+
+    /**
+     * 获取一个clusterPool 对象
+     *
+     * @return
+     */
+    public static JedisCluster getCluster() {
+        if (clusterPool == null) {
+            synchronized (JedisPoolManager.class) {
+                if (clusterPool == null) {
+                    createPool();
+                    System.out.println("创建JedisCluster完毕");
+                }
+            }
+        }
+        return clusterPool;
     }
 
     /**
@@ -104,6 +124,6 @@ public class JedisPoolManager {
      * @param jedis
      */
     public static void destroy(Jedis jedis) {
-        sentinelPool.destroy();
+        pool.destroy();
     }
 }
--- src/main/java/jedispool/Test1.java
+++ src/main/java/jedispool/Test1.java
@@ -1,16 +1,21 @@
 package jedispool;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 
 /**
  * redis 主从，哨兵，集群
  * https://blog.csdn.net/liuchuanhong1/article/details/53206028
  * https://www.cnblogs.com/lgeng/p/6623336.html
  * https://www.cnblogs.com/xifenglou/p/8372447.html
+ * https://www.cnblogs.com/tommy-huang/p/6240083.html
  */
 public class Test1 {
     public static void main(String[] args) {
-        Jedis jedis = JedisPoolManager.getJedis();
-        System.out.println(jedis.info());
+//        Jedis jedis = JedisPoolManager.getJedis();
+//        System.out.println(jedis.info());
+
+        JedisCluster clusterPool = JedisPoolManager.getCluster();
+        System.out.println(clusterPool.get("a"));
     }
 }
