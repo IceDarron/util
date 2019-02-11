720b847946c4a6cb61c78b0ff76633603706d5bd
rxn <darron_r@163.com>
2019-02-11 16:22:41 +0800

    zookeeper分布式锁

---
+++ src/main/java/distributedLock/zookeeper/Executor.java
@@ -0,0 +1,46 @@
+package distributedLock.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+
+import java.util.concurrent.TimeUnit;
+
+public class Executor {
+
+    final int sleepTime = 5000;
+
+    public Object execute(CuratorFramework client) {
+        IDistributedLockZookeeper iDistributedLockZookeeper = new LockZookeeperImpl(client, "TEST_DISTRIBUTED_LOCK", new ICallback() {
+            public Object onGetLock() throws InterruptedException {
+                System.out.println(Thread.currentThread().getName() + ":getLock");
+                Thread.currentThread().sleep(sleepTime);
+                System.out.println(Thread.currentThread().getName() + ":sleeped:" + sleepTime);
+                return null;
+            }
+
+            public Object onTimeout() throws InterruptedException {
+                System.out.println(Thread.currentThread().getName() + ":timeout");
+                return null;
+            }
+        });
+        int getLock = 0;
+        try {
+            getLock = iDistributedLockZookeeper.acquire(new Long(1000L), TimeUnit.MILLISECONDS);
+            if (getLock == 1) {
+                System.out.println(((LockZookeeperImpl) iDistributedLockZookeeper).getiCallback().onGetLock());
+            } else {
+                System.out.println(((LockZookeeperImpl) iDistributedLockZookeeper).getiCallback().onTimeout());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (getLock == 1) {
+                    iDistributedLockZookeeper.release();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/distributedLock/zookeeper/Test.java
+++ src/main/java/distributedLock/zookeeper/Test.java
@@ -1,10 +1,13 @@
 package distributedLock.zookeeper;
 
+import druiddatasource.PropertiesManager;
+import jedispool.JedisUtils;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
@@ -12,7 +15,8 @@ public class Test {
     public static void main(String[] args) {
         // 创建zookeeper客户端
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
-        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+//        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("10.4.59.141:2181", retryPolicy);
         client.start();
 
         // zookeeper通讯测试
@@ -32,38 +36,8 @@ public class Test {
 
 
         // zookeeper分布式锁测试
-        final int sleepTime = 5000;
-        IDistributedLockZookeeper iDistributedLockZookeeper = new LockZookeeperImpl(client, "TEST_DISTRIBUTED_LOCK", new ICallback() {
-            public Object onGetLock() throws InterruptedException {
-                System.out.println(Thread.currentThread().getName() + ":getLock");
-                Thread.currentThread().sleep(sleepTime);
-                System.out.println(Thread.currentThread().getName() + ":sleeped:" + sleepTime);
-                return null;
-            }
-
-            public Object onTimeout() throws InterruptedException {
-                System.out.println(Thread.currentThread().getName() + ":timeout");
-                return null;
-            }
-        });
-        int getLock = 0;
-        try {
-            getLock = iDistributedLockZookeeper.acquire(new Long(1000L), TimeUnit.MILLISECONDS);
-            if (getLock == 1) {
-                System.out.println(((LockZookeeperImpl) iDistributedLockZookeeper).getiCallback().onGetLock());
-            } else {
-                System.out.println(((LockZookeeperImpl) iDistributedLockZookeeper).getiCallback().onTimeout());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (getLock == 1) {
-                    iDistributedLockZookeeper.release();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        Executor executor = new Executor();
+        executor.execute(client);
     }
+
 }
