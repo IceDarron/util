4332fe06e7fe7e889b416b99d49cf85614d3ca4e
IceDarron <darron_r@163.com>
2019-02-24 14:41:48 +0800

    SocketChannel ServerSocketChannel

--- src/main/java/nio/SocketChannel/WebClient.java
+++ src/main/java/nio/SocketChannel/WebClient.java
@@ -10,13 +10,11 @@ public class WebClient implements Runnable {
     @Override
     public void run() {
         try {
-            // 1.通过SocketChannel的open()方法创建一个SocketChannel对象
+            // 通过SocketChannel的open()方法创建一个SocketChannel对象 连接到远程服务器（连接此通道的socket）
             SocketChannel socketChannel = SocketChannel.open();
-
-            // 2.连接到远程服务器（连接此通道的socket）
             socketChannel.connect(new InetSocketAddress("127.0.0.1", 3333));
 
-            // 3.创建写数据缓存区对象
+            // 创建写数据缓存区对象
             ByteBuffer writeBuffer = ByteBuffer.allocate(128);
             writeBuffer.put(("WebClient`s msg" + Thread.currentThread().getId()).getBytes());
             writeBuffer.flip();
@@ -25,11 +23,8 @@ public class WebClient implements Runnable {
             // 创建读数据缓存区对象
             ByteBuffer readBuffer = ByteBuffer.allocate(128);
             socketChannel.read(readBuffer);
-
-            StringBuffer sb = new StringBuffer();
-
-            // 4.将Buffer从写模式变为可读模式
             readBuffer.flip();
+            StringBuffer sb = new StringBuffer();
             while (readBuffer.hasRemaining()) {
                 sb.append((char) readBuffer.get());
             }
